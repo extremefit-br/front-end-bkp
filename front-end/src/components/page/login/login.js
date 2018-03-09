0a500b0f1eeb8b824b184b5f1a2d105138e720f9
rplandrade <rplandrade@gmail.com>
2018-03-09 18:55:25 -0300

    componentes

--- front-end/src/components/page/login/login.js
+++ front-end/src/components/page/login/login.js
@@ -2,27 +2,14 @@ import React from 'react'
 import './login.css'
 import LogoVidaSaudavel from './logo_vida_saudavel.png'
 import LogoSESI from './sesi.png'
-import FaUser from 'react-icons/lib/fa/user'
-import FaLock from 'react-icons/lib/fa/lock'
-import Input from '../../input/input.js'
-import Button from '../../button/button.js'
-
+import Form from '../../form/form.js'
 
 const Login = props => (
     <section className = "login">
         <span>
             <img className='logo-app' src={LogoVidaSaudavel} alt="" />
         </span>
-        <form>
-            <div>
-                <FaUser /> <Input />
-            </div>
-            <div>
-                <FaLock /> <Input />
-            </div>
-            <a className='link-esqueci-senha'>Esqueci minha senha</a>
-            <Button />
-        </form>  
+        <Form /> 
     </section>
 )
 
